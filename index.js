1c99e1c0c6ca94c9597c0d26388811a57d716d48
Aida Dieudonne <aidaharing10@gmail.com>
2024-09-04 19:25:14 -0600

    I completed this assignment

--- index.js
+++ index.js
@@ -9,15 +9,6 @@ function introductionWithLanguage(name, language){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-const user = "Gracie";
-const site = "Javascript";
-function introductionWithLanguageOptional(user, site = "Javascript") {
-    return `Hi, my name is ${user} and I am learning to program in ${site}.`;
-}
-
-const program = "Python"
-const person = "Gracie"
-let greeting = introductionWithLanguageOptional ("Gracie", "Python" );
-function introductionWithLanguageOptional(person, Python) {
-    return `Hi, my name is ${person} and I am learning to program in ${program}.`;
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
